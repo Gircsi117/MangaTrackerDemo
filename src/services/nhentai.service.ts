adedd19b83d53a326cc338126dbc8c3337209df5
Gircsi117 <gilianerik1107@gmail.com>
2026-04-19 13:24:37 +0200

    feat: enhance NHentaiService with chapter retrieval and caching mechanisms

--- src/services/nhentai.service.ts
+++ src/services/nhentai.service.ts
@@ -4,9 +4,13 @@ import { ListParams, List } from "../types/list.types";
 import {
   Chapter,
   ChapterContent,
+  ChapterPage,
+  ChapterSlug,
   Manga,
   MangaPageConstructor,
 } from "../types/manga.type";
+import { parse, HTMLElement } from "node-html-parser";
+import { v4 as uuidv4 } from "uuid";
 
 class NHentaiService extends MangaPage {
   public static readonly id: string = "nhentai";
@@ -16,7 +20,7 @@ class NHentaiService extends MangaPage {
   public static readonly logoUrl: string = "https://nhentai.net/logo.svg";
 
   private static readonly axios = axios.create({
-    baseURL: this.baseUrl + "/api/v2",
+    baseURL: this.baseUrl,
     headers: {
       Referer: this.referer,
       "User-Agent": this.userAgent,
@@ -27,6 +31,22 @@ class NHentaiService extends MangaPage {
     return `https://nhentai.net/g/${this.slug}`;
   }
 
+  private async getChapterPage(): Promise<HTMLElement | null> {
+    try {
+      const res = await NHentaiService.axios({
+        method: "GET",
+        url: `/g/${this.slug}`,
+      });
+
+      const root = parse(res.data);
+
+      return root;
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  }
+
   public static async search(params: ListParams): Promise<List<Manga>> {
     try {
       const { limit = 20, offset = 0, query } = params;
@@ -35,7 +55,7 @@ class NHentaiService extends MangaPage {
 
       const res = await this.axios({
         method: "GET",
-        url: query ? "/search" : "/galleries",
+        url: query ? "/api/v2/search" : "/api/v2/galleries",
         params: {
           query,
           page: page,
@@ -74,6 +94,26 @@ class NHentaiService extends MangaPage {
 
   public async getManga(): Promise<Manga | null> {
     try {
+      if (this.manga) return this.manga;
+
+      const res = await NHentaiService.axios({
+        method: "GET",
+        url: `/api/v2/galleries/${this.slug}`,
+      });
+
+      const data = res.data;
+      console.log(data);
+
+      this.manga = {
+        id: uuidv4(),
+        slug: this.slug,
+        title: data.title.english || data.title.japanese || data.title.pretty,
+        description: "",
+        author: "",
+        type: "manga",
+        coverUrl: `https://t1.nhentai.net/${data.cover.path}`,
+      };
+
       return this.manga;
     } catch (error) {
       console.error(error);
@@ -83,17 +123,55 @@ class NHentaiService extends MangaPage {
 
   public async getChapters(): Promise<Chapter[]> {
     try {
-      return [];
+      if (this.chapters.length) return this.chapters;
+      this.chapters = [
+        {
+          id: uuidv4(),
+          slug: "",
+          number: 1,
+          title: "Pages",
+          publishedAt: null,
+        },
+      ];
+
+      return this.chapters;
     } catch (error) {
       console.error(error);
       return [];
     }
   }
 
-  public async getChapterContent(): Promise<ChapterContent> {
+  public async getChapterContent(
+    chapterSlug: ChapterSlug,
+  ): Promise<ChapterContent> {
     try {
+      const res = await NHentaiService.axios({
+        method: "GET",
+        url: `/api/v2/galleries/${this.slug}`,
+      });
+
+      const data = res.data;
+
+      console.log(data.pages);
+
+      const pages: ChapterPage[] = data.pages.map(
+        (page: any, index: number) => {
+          const pageData: ChapterPage = {
+            id: uuidv4(),
+            index: page.number,
+            imageUrl: `https://i${(index % 4) + 1}.nhentai.net/${page.path}`,
+            width: page.width,
+            height: page.height,
+          };
+
+          return pageData;
+        },
+      );
+
+      console.log("pages", pages);
+
       return {
-        pages: [],
+        pages: pages,
         currentChapter: null,
         nextChapter: null,
         prevChapter: null,
